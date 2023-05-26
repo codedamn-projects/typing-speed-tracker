c9e7ba3ef3746e4f34837987667f88e5b18734d0
Harsh <harshchauhan6176@gmail.com>
2023-05-26 09:05:55 +0530

    Added the functionality to get WPM.

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const RANDOM_QUOTE_API_URL = "https://api.quotable.io/random";
 const quoteDisplayElement = document.getElementById("quoteDisplay");
 const quoteInputElement = document.getElementById("quoteInput");
 const timerElement = document.getElementById("timer");
+const wpmElement = document.getElementById("wpm");
+var ult_startTime = new Date();
 
 quoteInputElement.addEventListener("input", () => {
   const quoteInput = quoteInputElement.value;
   const quoteDisplay = quoteDisplayElement.querySelectorAll("span");
+  wpmElement.innerHTML = getWPM() + " WPM";
 
   let correct = true;
   quoteDisplay.forEach((characterSpan, ind) => {
@@ -33,10 +36,11 @@ function getRandomQuote() {
     .then((response) => response.json())
     .then((data) => data.content);
 }
-
 async function renderNewQuote() {
   const quote = await getRandomQuote();
   quoteDisplayElement.innerHTML = "";
+  wpmElement.innerHTML = "0 WPM";
+  ult_startTime = new Date();
   quote.split("").forEach((character) => {
     const charSpan = document.createElement("span");
     charSpan.innerHTML = character;
@@ -53,6 +57,14 @@ function startTimer() {
   setInterval(() => {
     timerElement.innerHTML = Math.floor((new Date() - startTime) / 1000);
   }, 1000);
+}
+
+function getWPM() {
+  let words = quoteInputElement.value.split(" ").length;
+  let time = new Date() - ult_startTime;
+  let wpm = words / (time / 60000);
+  wpm = wpm.toFixed(2);
+  return wpm;
 }
 
 renderNewQuote();
